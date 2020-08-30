d7d7d55a4f722a2fa1478a5a7b49375d19fdc801
yuandongju <yuandongju@58ganji.com>
2020-08-30 12:34:45 +0800

    edit_room頁面select

--- web/js/edit_room.js.php
+++ web/js/edit_room.js.php
@@ -15,7 +15,10 @@ if ($use_strict)
 
 $(document).on('page_ready', function() {
 
-
+  $('select#new_area').wrap('<div></div>').select2({// 开始时间段
+    width:130,
+    minimumResultsForSearch: -1
+  })
 
 });
 
